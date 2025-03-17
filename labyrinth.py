c55bd9fb267394b3aac990b8549d14bcefa2ec99
Yoten <juprellecamille@gmail.com>
2025-03-17 22:07:23 +0100

    removed unnecessary

--- labyrinth.py
+++ labyrinth.py
@@ -1,4 +1,4 @@
-from typing import Self, Any
+from typing import Any
 from base import Stack
 
 class labyrinth:
@@ -7,8 +7,8 @@ class labyrinth:
 
     Methodes :
         - .affiche : afficher le labyrinthe
-        - .nbligne : donne le nombre de lignes
-        - .nbcolonne : donne le nombre de colonne
+        - .nb_ligne : donne le nombre de lignes
+        - .nb_colonne : donne le nombre de colonne
         - .depart : donne les coordonnées du départ
         - .arrivee : donne les coordonnées de l'arrivée
         - .est_valide : vérifie la véracité des coordonées dans un laryrinthe
@@ -17,17 +17,17 @@ class labyrinth:
         - .liste_voisines_libres : donne les cases libres à proximité
     
     """
-    def __init__(self : Self, li : list):
+    def __init__(self , li : list):
         self.tab = li
 
-    def affiche(self: Self) -> list:
+    def affiche(self ) -> list:
         """
         Affiche le labyrinthe
         """
         for inner_list in self.tab:
             print(inner_list)
 
-    def nblignes(self : Self) -> int:
+    def nb_ligne(self ) -> int:
         """
         Donne le nombre de lignes
         """
@@ -36,7 +36,7 @@ class labyrinth:
             count += 1
         return count
     
-    def nbcolonne(self : Self) -> int :
+    def nb_colonne(self) -> int :
         """
         Donne le nombre de colonne
         """
@@ -45,7 +45,7 @@ class labyrinth:
             count+= 1
         return count
     
-    def depart(self : Self) -> int :
+    def depart(self ) -> int :
         """
         Donne le tuple du point de départ
         """
@@ -56,7 +56,7 @@ class labyrinth:
                     return i, j
             i += 1 
 
-    def arrivee(self : Self) -> int:
+    def arrivee(self) -> int:
         """
         Donne le tuple du point d'arrivée
         """
@@ -67,15 +67,15 @@ class labyrinth:
                     return i, j
             i += 1
 
-    def est_valide(self : Self, i : int, j: int) -> bool :
+    def est_valide(self , i : int, j: int) -> bool :
         """
         Vérifie la véracité des coordonées dans un laryrinthe
         """
-        if 0 <= i <= self.nblignes and 0 <= j <= self.nbcolonne() :
+        if 0 <= i <= self.nb_ligne and 0 <= j <= self.nb_colonne() :
             return True
         return False
     
-    def nb_cases_vides(self : Self) -> int:
+    def nb_cases_vides(self ) -> int:
         """
         Compte le nombre de cases vides dans un labyrinthe
         """
@@ -88,18 +88,18 @@ class labyrinth:
             i += 1
         return nbempty
 
-    def est_visitee(self : Self, i : int, j : int) -> None:
+    def est_visitee(self , i : int, j : int) -> None:
         """
         Marque une case comme visité en changeant le chiffre en 4
         """
         self.tab[i][j] = 4
 
-    def liste_voisines_libres(self : Self, i: int, j : int) -> list :
+    def liste_voisines_libres(self , i: int, j : int) -> list :
         """
         Donne une liste des coordonnées des cases voisines disponibles 
         """
         available = []
-        if j+1 <= self.nbcolonne()-1 :
+        if j+1 <= self.nb_colonne()-1 :
             if self.tab[i][j+1] == 0 or self.tab[i][j+1] == 3:
                 available.append((i, j+1))
         if j-1 >= 0: 
@@ -108,14 +108,14 @@ class labyrinth:
         if i-1 >= 0: 
             if self.tab[i-1][j] == 0 or self.tab[i-1][j] == 3:
                 available.append((i-1, j))
-        if i+1 <= self.nblignes()-1:
+        if i+1 <= self.nb_ligne()-1:
             if self.tab[i+1][j] == 0 or self.tab[i+1][j] == 3:
                 available.append((i+1, j))       
         if available != []:
             return available
         return None
     
-    def case_fausse(self : Self, i : int, j : int) -> None :
+    def case_fausse(self , i : int, j : int) -> None :
         """
         Remplace le numéro par 5 d'une case menant sur une impasse
         """
@@ -135,60 +135,26 @@ tab2 = [[1, 1, 1, 1, 1, 1, 1],
 
 
 lab1 = labyrinth(tab2)
-# lab1.affiche()
-# print(lab1.nblignes())
-# print(lab1.nbcolonne())
-# print(lab1.depart())
-# print(lab1.arrivee())
-# print(lab1.est_valide(4, 5))
-# print(lab1.est_valide(12, -3))
-# print(lab1.nb_cases_vides())
-# print(lab1.liste_voisines_libres(2, 0))
-
-clab1 = lab1
-sol = Stack()
-x, y = clab1.depart()
-w, z = clab1.arrivee()
-clab1.est_visitee(x, y)
-l = clab1.liste_voisines_libres(x, y)
 
 
-while x != w or y != z: 
-    l = clab1.liste_voisines_libres(x, y)
-    x, y = l[0]
+if __name__ == '__main__':
+    clab1 = lab1
+    sol = Stack()
+    x, y = clab1.depart()
+    w, z = clab1.arrivee()
     clab1.est_visitee(x, y)
-    sol.put((x, y))
     l = clab1.liste_voisines_libres(x, y)
-    if clab1.liste_voisines_libres(x, y) == None and (x, y) != (w, z):
-        while clab1.liste_voisines_libres(x, y) == None:
-            x, y = sol.get()
+    while x != w or y != z: 
+        l = clab1.liste_voisines_libres(x, y)
+        x, y = l[0]
+        clab1.est_visitee(x, y)
+        sol.put((x, y))
+        l = clab1.liste_voisines_libres(x, y)
+        if clab1.liste_voisines_libres(x, y) == None and (x, y) != (w, z):
+            while clab1.liste_voisines_libres(x, y) == None:
+                x, y = sol.get()
 
 
-while not sol.is_empty():
-        print(sol.get())
+    while not sol.is_empty():
+            print(sol.get())
 
-
-
-
-
-#etape 4 : 2. il n'y a pas de solutions
-# while clab1.liste_voisines_libres(x, y) != None :
-#     l = clab1.liste_voisines_libres(x, y)
-#     x, y = l[0], l[1]
-#     clab1.est_visitee(x, y)
-#     sol.put(f"({x}, {y})")
-#     l = clab1.liste_voisines_libres(x, y)
-# if x == w and y == z : 
-#     while not sol.is_empty():
-#         print(sol.get())
-# else :
-#     pass
-# pas finito
-
-
-#tuple = n-uplet
-tu = (3,4,5)
-#ou tu = 3,4,5
-x, y, z = tu
-print(tu)
-print(tu[0])
